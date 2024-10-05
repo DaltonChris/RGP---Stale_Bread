72997e2f04e3fc0139eae71837041a862818cc82
DuckPeak <dylan.coon@student.torrens.edu.au>
2024-10-06 00:38:05 +1000

    Bread Score fixed
    Bread score not gives 10 score as intended, removed 2nd player collider resulting in double score. Updated the build.exe

--- Assets/Scripts/ScoreItem.cs
+++ Assets/Scripts/ScoreItem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ScoreItem : MonoBehaviour
 {
-    public int scoreValue = 10; // The amount of score this item gives
+    private int scoreValue = 10; // The amount of score this item gives
     public AudioClip ItemPickUpSFX;
     public AudioSource audioSource;
     // When the player collides with the item, add the score and destroy the item
